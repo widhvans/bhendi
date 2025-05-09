c45de8d71b74acbd32163468adc1058b7d15131e
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-10 00:33:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,9 +6,11 @@ class Database:
         self.client = MongoClient(config.MONGO_URI)
         self.db = self.client['telegram_bot']
         self.files = self.db['files']
+        self.index_state = self.db['index_state']
         
         # Create index for faster searches
         self.files.create_index([('chat_id', 1), ('file_name', 'text')])
+        self.index_state.create_index([('chat_id', 1)])
     
     def save_file(self, chat_id, file_name, file_id):
         self.files.update_one(
@@ -32,6 +34,20 @@ class Database:
             'chat_id': chat_id,
             '$text': {'$search': search_term}
         }))
+    
+    def get_file_count(self, chat_id):
+        return self.files.count_documents({'chat_id': chat_id})
+    
+    def save_indexed_offset(self, chat_id, offset):
+        self.index_state.update_one(
+            {'chat_id': chat_id},
+            {'$set': {'last_offset': offset}},
+            upsert=True
+        )
+    
+    def get_last_indexed_offset(self, chat_id):
+        state = self.index_state.find_one({'chat_id': chat_id})
+        return state.get('last_offset', 0) if state else 0
     
     def close(self):
         self.client.close()
