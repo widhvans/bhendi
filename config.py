9945f03d3855c24c4528b2ba7fc4fa301a97e7cc
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-10 10:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"
 MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "sarkaar"
+LOG_FILE = "bot.log"
